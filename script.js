3d5348dce0d288676f7a1bdf05a39c1428ea1670
missycoder mylan <noreen.taha@outlook.com>
2024-02-07 04:29:08 +0000

    added toggleBtn

--- script.js
+++ script.js
@@ -13,7 +13,23 @@ document.addEventListener("DOMContentLoaded", function () {
         // adding markers to the map for the search results
         addMarkersToMap(data, searchLayer, map);
 
-    
+    });
+
+    document.querySelector("#toggleSearchBtn").addEventListener("click", function(){
+        
+        const searchContainer = document.querySelector("#search-container");
+        const style = window.getComputedStyle(searchContainer);
+        // if the search container is already visible, we'll hide it
+        if (style.display != "none") {
+            searchContainer.style.display = "none";
+        } else {
+              // otherwise, show it
+              searchContainer.style.display = 'block';
+        }
+
+      
     })
+
+    
 
 });
